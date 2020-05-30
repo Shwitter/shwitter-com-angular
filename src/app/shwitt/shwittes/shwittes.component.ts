f4647f18ea06f74efd7ecdb66e63ad99dcd0f7ef
bsanaia <b.sanaia@hotelhub.ge>
2020-05-30 13:57:52 +0400

    image upload

--- src/app/shwitt/shwittes/shwittes.component.ts
+++ src/app/shwitt/shwittes/shwittes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ShwittService} from '../shwittService/shwitt.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
@@ -17,12 +17,17 @@ export class ShwittesComponent implements OnInit {
   shwitts;
   shwittForm: FormGroup;
   asd: any;
-  
+
 
   urls = [];
   arr = [1, 2, 3, 4, 5];
 
-  constructor(private shwittService: ShwittService, private router: Router, private authService: AuthService,private http: HttpClient) { }
+  constructor(private shwittService: ShwittService,
+              private router: Router,
+              private authService: AuthService,
+              private http: HttpClient,
+              private cd: ChangeDetectorRef) {
+  }
 
   ngOnInit(): void {
     if (!this.authService.isAuthenticated()) {
@@ -36,9 +41,6 @@ export class ShwittesComponent implements OnInit {
       shweetimage: new FormControl('', [Validators.required])
     });
 
-    this.shwittForm.get('shweetimage').valueChanges.subscribe(res => {
-      console.log(res);
-    })
   }
 
   shwitt(event) {
@@ -50,36 +52,37 @@ export class ShwittesComponent implements OnInit {
     //   }
     // })
 
-    let formData = new FormData();
+    const formData = new FormData();
     formData.append('body', this.shwittForm.value.body);
     formData.append('shweetimage', this.shwittForm.value.shweetimage);
 
-    if (event.target.files && event.target.files[0]) {
-      var reader = new FileReader();
+    this.http.post(`http://api.shwitter.localhost/shweet/create`, formData).subscribe(res => {
+      this.getShwitts();
+    });
 
-      reader.readAsDataURL(event.target.files[0]);
 
-      reader.onload = (event) => {
-        this.asd = event.target.result;
-      }
+  }
+
+  ImageChange(event) {
+    const reader = new FileReader();
+
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+      console.log(reader.result);
+      reader.onload = () => {
+        this.shwittForm.patchValue({
+          shweetimage: reader.result
+        });
+
+        this.cd.markForCheck();
+      };
     }
-
-    console.log(event.target.files);
-
-    // this.http.post(`http://api.shwitter.localhost/shweet/create`, formData).subscribe(res => {
-    //     this.getShwitts();
-    // });
-
-
   }
 
   getShwitts() {
-    this.shwittService.getShwitts().subscribe((res : any) => {
+    this.shwittService.getShwitts().subscribe((res: any) => {
       this.shwitts = res;
-    })
-  }
-
-  uploadImg(event) {
-
+    });
   }
 }
