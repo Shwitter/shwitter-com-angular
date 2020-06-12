eb1658331186e2b420cd579ac00c2c947e395292
nika <n_khonelidze2@cu.edu.ge>
2020-06-13 02:28:13 +0400

    make likes work

--- src/app/shwitt/shwittService/shwitt.service.ts
+++ src/app/shwitt/shwittService/shwitt.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class ShwittService {
-  baseUrl = 'http://api.shwitter.localhost/';
+  baseUrl = '/http://api.shwitter.localhost/';
   token;
   constructor(private http: HttpClient) {
   }
